bbdd9afb2cf7d66bfdd6b53575b269fc25226258
NiklasVasa <81626057+NiklasVasa@users.noreply.github.com>
2021-05-25 11:08:35 +0200

    Update sqlConnection.js

--- SQL/sqlConnection.js
+++ SQL/sqlConnection.js
@@ -2,11 +2,15 @@ var sql = require("msnodesqlv8");
 
 //connString är strängen som visar var databasen finns och ger all information för att man ska kunna koppla sig till den.
 //msnodesql används för att kunna utnyttja windows-authentication till databasen
+
+//connString is the connection string
 let connString = "server=APHK-SRV104;Database=Skjutsgossen8TempDatabase;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
 
 //Här hämtas observationstiden. Alltså det datum som vi vill *BÖRJA* hämta data ifrån för att hålla databasen uppdaterad. 
 //Det är det senaste inrapporterade värdet. 
+
+//Gets the observation time, the date that you want to start collecting data from the database. 
 function getObsTime(query, callback) {
 
   sql.query(connString, query, (err, rows) => {
@@ -20,6 +24,8 @@ function getObsTime(query, callback) {
 
 
 //Här stoppar vi in nya värden i databasen.
+
+//Inputting values into the database
 function insertVal(query) {
   sql.query(connString, query, (err, rows) => {
     if (err) {
